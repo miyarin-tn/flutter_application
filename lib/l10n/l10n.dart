153d8a6a8f9f1da2c3f35373c85395268753eaee
Thinh Nguyen <thinh.nguyen@dirox.net>
2022-07-02 20:42:56 +0700

    test translate

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class L10n {
+  static final all = [
+    const Locale('en', ''), // English, no country code
+    const Locale('vi', ''), // Vietnamese, no country code
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application/l10n/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_application/provider/locale.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,22 +14,124 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => LocaleProvider(),
+      builder: (context, child) {
+        final provider = Provider.of<LocaleProvider>(context);
+        Color color = Theme.of(context).primaryColor;
+
+        Widget titleSection = Container(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            children: [
+              Expanded(
+                /*1*/
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /*2*/
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: const Text(
+                        'Oeschinen Lake Campground',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Kandersteg, Switzerland',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              /*3*/
+              Icon(
+                Icons.star,
+                color: Colors.red[500],
+              ),
+              const Text('41'),
+            ],
+          ),
+        );
+
+        Widget buttonSection = Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildButtonColumn(color, Icons.call, 'CALL'),
+            _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+            _buildButtonColumn(color, Icons.share, 'SHARE'),
+          ],
+        );
+
+        Widget textSection = const Padding(
+          padding: EdgeInsets.all(32),
+          child: Text(
+            'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+            softWrap: true,
+          ),
+        );
+
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter layout demo',
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            locale: provider.locale,
+            supportedLocales: L10n.all,
+            home: MyHomePage(title: 'Flutter Demo Home Page'));
+        // home: Scaffold(
+        //     appBar: AppBar(
+        //       title: const Text('test'),
+        //     ),
+        //     body: ListView(
+        //       children: <Widget>[
+        //         Center(
+        //           child: Text(AppLocalizations.of(context)!.testa),
+        //         ),
+        //         Image.asset(
+        //           'images/lake.jpg',
+        //           width: 600,
+        //           height: 240,
+        //           fit: BoxFit.cover,
+        //         ),
+        //         titleSection,
+        //         buttonSection,
+        //         textSection,
+        //       ],
+        //     )));
+      });
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -69,6 +176,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    // Locale myLocale = Localizations.localeOf(context);
+    final provider = Provider.of<LocaleProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -95,6 +205,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextButton(
+                onPressed: () => {provider.setLocale(const Locale('en'))},
+                child: const Text('English')),
+            TextButton(
+                onPressed: () => {provider.setLocale(const Locale('vi'))},
+                child: const Text('Vietnamese')),
+            Text(AppLocalizations.of(context)!.testa),
             const Text(
               'You have pushed the button this many times:',
             ),

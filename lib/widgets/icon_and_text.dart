0211fb6d3bbef0a2b83aa1b66fc195ce61be598b
Thinh Nguyen <thinh.nguyen@dirox.net>
2022-07-07 20:55:42 +0700

    header and slider

---
+++ lib/widgets/icon_and_text.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application/widgets/small_text.dart';
+
+class IconAndText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color? color;
+  final Color? iconColor;
+  const IconAndText({
+    Key? key,
+    required this.icon,
+    required this.text,
+    this.color = const Color(0xFFccc7c5),
+    this.iconColor = const Color(0xFF332d2b),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconColor,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        SmallText(
+          text: text,
+          color: color,
+        ),
+      ],
+    );
+  }
+}
